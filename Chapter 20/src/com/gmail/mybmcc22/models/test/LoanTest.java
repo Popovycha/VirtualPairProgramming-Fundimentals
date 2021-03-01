c7ff0a1c0d42a05679d376d34859554689c09cf8
Popovycha <artem220397@gmail.com>
2021-02-28 20:23:51 -0500

    Add files via upload

---
+++ Chapter 20/src/com/gmail/mybmcc22/models/test/LoanTest.java
@@ -0,0 +1,10 @@
+package com.gmail.mybmcc22.models.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoanTest {
+
+   public void testDueDate() {
+    fail("not yet implemented");
+   }
+}
---
+++ Chapter 20/src/com/gmail/mybmcc22/models/test/LoanTestTest.java
@@ -0,0 +1,37 @@
+package com.gmail.mybmcc22.models.test;
+
+import com.gmail.mybmcc22.models.Book;
+import com.gmail.mybmcc22.models.Customer;
+import com.gmail.mybmcc22.models.Loan;
+import com.gmail.mybmcc22.utilities.Gender;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanTestTest {
+
+    @Test
+    void testDueDate() {
+
+        Book book = new Book(0," "," ", " ", " ",
+                0);
+        Customer customer = new Customer("","","","",
+        "","",0, Gender.MALE);
+
+        Loan loan = new Loan(0, customer, book);
+
+        GregorianCalendar gcExpected = new GregorianCalendar();
+        gcExpected.add(GregorianCalendar.DAY_OF_WEEK, 14);
+
+        //gcActual times stamp from the point of creation
+        GregorianCalendar gcActual = new GregorianCalendar();
+        gcActual.setTime(loan.getDueDate());
+
+        assertEquals(gcExpected.get(GregorianCalendar.YEAR), gcActual.get(GregorianCalendar.YEAR));
+        assertEquals(gcExpected.get(GregorianCalendar.MONTH), gcActual.get(GregorianCalendar.MONTH));
+        assertEquals(gcExpected.get(GregorianCalendar.DAY_OF_MONTH), gcActual.get(GregorianCalendar.DAY_OF_MONTH));
+    }
+}
