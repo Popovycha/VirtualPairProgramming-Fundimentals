dc2eacc59e2edde132c221857199f56b927a619a
Popovycha <artem220397@gmail.com>
2021-03-01 18:48:14 -0500

    Add files via upload

---
+++ Chapter 21/src/com/gmail/mybmcc22/models/BookTest.java
@@ -0,0 +1,24 @@
+package com.gmail.mybmcc22.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BookTest {
+
+    @Test
+    public void test2EqualsBooks() {
+        Book book1 = new Book(100, "","","","",1);
+        Book book2 = new Book(100, "","","","",1);
+
+        assertTrue(book1.equals(book2));
+
+    }
+    @Test
+    public void test2NonEqualBooks() {
+        Book book1 = new Book(100, "","","","",1);
+        Book book2 = new Book(101, "","","","",1);
+
+        assertFalse(book1.equals(book2));
+    }
+}
---
+++ Chapter 21/src/com/gmail/mybmcc22/models/test/LoanTest.java
@@ -0,0 +1,11 @@
+package com.gmail.mybmcc22.models.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class LoanTest {
+
+   public void testDueDate() {
+    fail("not yet implemented");
+   }
+}
